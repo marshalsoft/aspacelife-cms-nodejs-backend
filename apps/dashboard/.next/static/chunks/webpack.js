9c10aa1fc2bac0b72bf2f9485f35b7f0463dadf3
Marshalsoft <marshallekene@hotmail.com>
2024-12-08 15:08:12 +0100

    update

--- apps/dashboard/.next/static/chunks/webpack.js
+++ apps/dashboard/.next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ba4ce9f224cd78c6"; }
+/******/ 		__webpack_require__.h = function() { return "2564bd28b75bebea"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
